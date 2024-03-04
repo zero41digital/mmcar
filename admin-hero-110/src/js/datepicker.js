4ce0937a0b59441a8c306bbe6366a79498b64fb2
ZERO41 Digital <zero41@zero41digital.com.br>
2024-03-04 16:04:03 -0300

    novo

--- admin-hero-110/src/js/datepicker.js
+++
@@ -1,7 +0,0 @@
-$('.datepicker').datepicker({   orientation: 'bottom left'});
-$('.datepicker-retail').datepicker({   orientation: 'auto'});
-$('.datepicker-property').datepicker({   orientation: 'auto'});
-
-$('.input-daterange input').datepicker({
-  orientation: 'auto'
-});
