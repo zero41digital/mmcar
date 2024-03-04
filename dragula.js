5df3b42238b2b77553c4a4ca7dcdc86e1149ab94
zero41digital <161201192+zero41digital@users.noreply.github.com>
2024-03-04 19:41:04 -0300

    Add files via upload

--- dragula.js
+++ dragula.js
@@ -1,1 +1,39 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=199)}({199:function(e,t,n){e.exports=n(200)},200:function(e,t){!function(){"use strict";$('[data-toggle="dragula"]').each(function(){var e=$(this),t={moves:function(t,n,r,o){return void 0===e.data("dragula-moves")||r.classList.contains(e.data("dragula-moves"))}},n=$(this).data("dragula-containers"),r=[];if(n)for(var o=0;o<n.length;o++)r.push(document.querySelector(n[o]));else r.push(e[0]);dragula(r,t).on("cloned",function(e,t){"TR"===e.nodeName&&$(e).children().each(function(e,n){$(n).width($(t).children().eq(e).outerWidth()).css("backgroundColor","#ffffff").css("padding",$(t).children().eq(e).css("padding"))})})})}()}});
+(function(){
+  'use strict';
+
+  $('[data-toggle="dragula"]').each(function() {
+    var element = $(this)
+    var options = {
+      moves: function(el, source, handle, sibling) {
+        return void 0 !== element.data('dragula-moves')
+          ? handle.classList.contains(element.data('dragula-moves'))
+          : true
+      }
+    }
+
+    var containers = $(this).data('dragula-containers')
+    var elements = []
+
+    if (containers) {
+      for (var i = 0; i < containers.length; i++) {
+        elements.push(document.querySelector(containers[i]))
+      }
+    }
+    else {
+      elements.push(element[0])
+    }
+
+    dragula(elements, options)
+      .on('cloned', function (el, original) {
+        if (el.nodeName === 'TR') {
+          $(el).children().each(function(index, cell) {
+            $(cell)
+              .width($(original).children().eq(index).outerWidth())
+              .css('backgroundColor', '#ffffff')
+              .css('padding', $(original).children().eq(index).css('padding'))
+          })
+        }
+      })
+  })
+
+})()
--- toastr.js
+++ toastr.js
@@ -1,1 +1,61 @@
-!function(t){var a={};function o(e){if(a[e])return a[e].exports;var r=a[e]={i:e,l:!1,exports:{}};return t[e].call(r.exports,r,r.exports,o),r.l=!0,r.exports}o.m=t,o.c=a,o.d=function(t,a,e){o.o(t,a)||Object.defineProperty(t,a,{enumerable:!0,get:e})},o.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},o.t=function(t,a){if(1&a&&(t=o(t)),8&a)return t;if(4&a&&"object"==typeof t&&t&&t.__esModule)return t;var e=Object.create(null);if(o.r(e),Object.defineProperty(e,"default",{enumerable:!0,value:t}),2&a&&"string"!=typeof t)for(var r in t)o.d(e,r,function(a){return t[a]}.bind(null,r));return e},o.n=function(t){var a=t&&t.__esModule?function(){return t.default}:function(){return t};return o.d(a,"a",a),a},o.o=function(t,a){return Object.prototype.hasOwnProperty.call(t,a)},o.p="/",o(o.s=215)}({215:function(t,a,o){t.exports=o(216)},216:function(t,a){!function(){"use strict";toastr.primary=function(t,a,o){return this.success(t,a,$.extend({iconClass:"toast-primary"},o))},$('[data-toggle="toastr"]').on("click",function(t){t.preventDefault();var a=$(this),o=a.data("toastr-type")||"success",e=a.data("toastr-message"),r=a.data("toastr-title"),s={closeButton:void 0!==a.data("toastr-close-button")&&a.data("toastr-close-button"),debug:!1,newestOnTop:void 0===a.data("toastr-newest-on-top")||a.data("toastr-newest-on-top"),progressBar:void 0===a.data("toastr-progress-bar")||a.data("toastr-progress-bar"),positionClass:void 0!==a.data("toastr-position-class")?a.data("toastr-position-class"):"toast-top-right",preventDuplicates:void 0!==a.data("toastr-prevent-duplicates")&&a.data("toastr-prevent-duplicates"),onclick:null,showDuration:void 0!==a.data("toastr-show-duration")?a.data("toastr-show-duration"):300,hideDuration:void 0!==a.data("toastr-hide-duration")?a.data("toastr-hide-duration"):1e3,timeOut:void 0!==a.data("toastr-time-out")?a.data("toastr-time-out"):5e3,extendedTimeOut:void 0!==a.data("toastr-extended-time-out")?a.data("toastr-extended-time-out"):1e3,showEasing:void 0!==a.data("toastr-show-easing")?a.data("toastr-show-easing"):"swing",hideEasing:void 0!==a.data("toastr-hide-easing")?a.data("toastr-hide-easing"):"linear",showMethod:void 0!==a.data("toastr-show-method")?a.data("toastr-show-method"):"fadeIn",hideMethod:void 0!==a.data("toastr-hide-method")?a.data("toastr-hide-method"):"fadeOut"};toastr[o](e,r,s)})}()}});
+(function(){
+  'use strict';
+
+  toastr.primary = function(message, title, optionsOverride) {
+    return this.success(message, title, $.extend({ iconClass: 'toast-primary' }, optionsOverride))
+  }
+
+  $('[data-toggle="toastr"]').on('click', function(e) {
+    e.preventDefault()
+    const element = $(this)
+    const type = element.data('toastr-type') || 'success'
+    const message = element.data('toastr-message')
+    const title = element.data('toastr-title')
+    const options = {
+      closeButton: void 0 !== element.data('toastr-close-button') 
+        ? element.data('toastr-close-button') 
+        : false,
+      debug: false,
+      newestOnTop: void 0 !== element.data('toastr-newest-on-top') 
+        ? element.data('toastr-newest-on-top') 
+        : true,
+      progressBar: void 0 !== element.data('toastr-progress-bar') 
+        ? element.data('toastr-progress-bar') 
+        : true,
+      positionClass: void 0 !== element.data('toastr-position-class') 
+        ? element.data('toastr-position-class') 
+        : "toast-top-right",
+      preventDuplicates: void 0 !== element.data('toastr-prevent-duplicates') 
+        ? element.data('toastr-prevent-duplicates') 
+        : false,
+      onclick: null,
+      showDuration: void 0 !== element.data('toastr-show-duration') 
+        ? element.data('toastr-show-duration') 
+        : 300,
+      hideDuration: void 0 !== element.data('toastr-hide-duration') 
+        ? element.data('toastr-hide-duration') 
+        : 1000,
+      timeOut: void 0 !== element.data('toastr-time-out') 
+        ? element.data('toastr-time-out') 
+        : 5000,
+      extendedTimeOut: void 0 !== element.data('toastr-extended-time-out') 
+        ? element.data('toastr-extended-time-out') 
+        : 1000,
+      showEasing: void 0 !== element.data('toastr-show-easing') 
+        ? element.data('toastr-show-easing') 
+        : 'swing',
+      hideEasing: void 0 !== element.data('toastr-hide-easing') 
+        ? element.data('toastr-hide-easing') 
+        : 'linear',
+      showMethod: void 0 !== element.data('toastr-show-method') 
+        ? element.data('toastr-show-method') 
+        : 'fadeIn',
+      hideMethod: void 0 !== element.data('toastr-hide-method') 
+        ? element.data('toastr-hide-method') 
+        : 'fadeOut'
+    }
+
+    toastr[type](message, title, options)
+  })
+
+})()
